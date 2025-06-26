093b7652a396cb109cf8f2080fcd123b1051f3fb
bhavyraheja <bhavy2002raheja@gmail.com>
2025-06-26 16:15:07 +0530

    all done back

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://exam-module-front.vercel.app/", credentials: true }));
 app.use(fileUpload());
 app.use("/uploads", express.static("uploads"));
 
